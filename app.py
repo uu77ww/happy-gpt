a360a2151ad68e2d232ca5c4a6e721aea778984a
joshra <joshracat@gmail.com>
2023-05-18 18:51:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,6 +27,9 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     reply_msg = '我只是無害的NPC，請不要傷害我'
+    msg = json_data['events'][0]['message']['text']
+    # 取出文字的前五個字元，轉換成小寫
+    ai_msg = msg[:6].lower()
     if ai_msg == 'hi ai:':
             # 將第六個字元之後的訊息發送給 OpenAI
             response = openai.Completion.create(
