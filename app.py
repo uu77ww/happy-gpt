556fbef5e5660de75478ca01b6d2326395ce8f05
joshra <joshracat@gmail.com>
2023-05-18 20:43:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,24 +26,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    ai_msg = msg[:6].lower()
-    if ai_msg == 'hi ai:':
-            # 將第六個字元之後的訊息發送給 OpenAI
-            response = openai.Completion.create(
-                model='text-davinci-003',
-                prompt=msg[6:],
-                max_tokens=256,
-                temperature=0.5,
-                )
-            # 接收到回覆訊息後，移除換行符號
-            reply_msg = TextSendMessage(text=response["choices"][0]["text"].replace('\n',''))
-
-    if msg == "午餐吃甚麼":
-        lunch_options = ['便當', '麵類', '飯類', '燉飯', '三明治']
-        reply_msg = TextSendMessage(text=random.choice(lunch_options))
-        #message = TextSendMessage(text='你剛剛是說 '+event.message.text+'嗎?')
-    
+    #message = TextSendMessage(text='你剛剛是說 '+event.message.text+'嗎?')
+    reply_msg = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, reply_msg)
 
 import os
