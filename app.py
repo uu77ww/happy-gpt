6a5aacdaf12f74609d9f97865d228a82e6e686ce
joshra <joshracat@gmail.com>
2023-05-25 21:16:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -27,157 +27,49 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     card = {
-          "type": "bubble",
-          "hero": {
-            "type": "image",
-            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
-            "size": "full",
-            "aspectRatio": "20:13",
-            "aspectMode": "cover",
-            "action": {
-              "type": "uri",
-              "uri": "http://linecorp.com/"
-            }
-          },
-          "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-              {
-                "type": "text",
-                "text": "八方雲集",
-                "weight": "bold",
-                "size": "xl"
+              "type": "bubble",
+              "hero": {
+                "type": "image",
+                "url": "https://lions-clubs.dev2.rib.tw/static/documents/images/Lions_Clubs_International.png",
+                "size": "full",
+                "aspectRatio": "1:1",
+                "aspectMode": "cover",
+                "action": {
+                  "type": "uri",
+                  "uri": "http://linecorp.com/"
+                }
               },
-              {
+              "body": {
                 "type": "box",
-                "layout": "baseline",
-                "margin": "md",
+                "layout": "vertical",
                 "contents": [
                   {
-                    "type": "icon",
-                    "size": "sm",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                  },
-                  {
-                    "type": "icon",
-                    "size": "sm",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                  },
-                  {
-                    "type": "icon",
-                    "size": "sm",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                  },
-                  {
-                    "type": "icon",
-                    "size": "sm",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png"
-                  },
-                  {
-                    "type": "icon",
-                    "size": "sm",
-                    "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png"
-                  },
-                  {
                     "type": "text",
-                    "text": "4.0",
-                    "size": "sm",
-                    "color": "#999999",
-                    "margin": "md",
-                    "flex": 0
+                    "text": "113年度獲獎獅友名單",
+                    "weight": "bold",
+                    "size": "xl"
                   }
                 ]
               },
-              {
+              "footer": {
                 "type": "box",
                 "layout": "vertical",
-                "margin": "lg",
                 "spacing": "sm",
                 "contents": [
                   {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Place",
-                        "color": "#aaaaaa",
-                        "size": "sm",
-                        "flex": 1
-                      },
-                      {
-                        "type": "text",
-                        "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
-                        "wrap": true,
-                        "color": "#666666",
-                        "size": "sm",
-                        "flex": 5
-                      }
-                    ]
-                  },
-                  {
-                    "type": "box",
-                    "layout": "baseline",
-                    "spacing": "sm",
-                    "contents": [
-                      {
-                        "type": "text",
-                        "text": "Time",
-                        "color": "#aaaaaa",
-                        "size": "sm",
-                        "flex": 1
-                      },
-                      {
-                        "type": "text",
-                        "text": "10:00 - 23:00",
-                        "wrap": true,
-                        "color": "#666666",
-                        "size": "sm",
-                        "flex": 5
-                      }
-                    ]
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                      "type": "uri",
+                      "label": "公文連結",
+                      "uri": "https://lions-clubs.dev2.rib.tw"
+                    }
                   }
-                ]
+                ],
+                "flex": 0
               }
-            ]
-          },
-          "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "spacing": "sm",
-            "contents": [
-              {
-                "type": "button",
-                "style": "link",
-                "height": "sm",
-                "action": {
-                  "type": "uri",
-                  "label": "CALL",
-                  "uri": "https://linecorp.com"
-                }
-              },
-              {
-                "type": "button",
-                "style": "link",
-                "height": "sm",
-                "action": {
-                  "type": "uri",
-                  "label": "WEBSITE",
-                  "uri": "https://linecorp.com"
-                }
-              },
-              {
-                "type": "box",
-                "layout": "vertical",
-                "contents": [],
-                "margin": "sm"
-              }
-            ],
-            "flex": 0
-          }
-        }
+            }
     msg = event.message.text
     if msg  == "發卡片":
         line_bot_api.reply_message(
