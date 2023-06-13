3d0a9050450d1f1a03195fa0e54c66d2610a27ed
許昀暄 <126781189+uu77ww@users.noreply.github.com>
2023-06-13 18:39:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,19 +2,14 @@ from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
-import os
-import random
-from ytmusicapi import YTMusic
 
 app = Flask(__name__)
 
 # 填入你的 Line Channel Access Token
-line_bot_api = LineBotApi('I86eRgDQEGoKvT5aXoefw9ekgJ4z2ACVKABcl9FK/JO1zbkWtwEYSjdzITlNNqSk0deRWhWVKGe3BvCef35jz7EDmBZVHTleu5/98I1CDuHP1IvglHx8t2YoT6WIu2Ica2E3TtEoftaprwIlEv4LOAdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('你的 Channel Access Token')
 
 # 填入你的 Line Channel Secret
-handler = WebhookHandler('c22f27f77d5d23fe3265b82966e7b02d')
-
-yt = YTMusic('AIzaSyDos01WcEoudVx-bcNekQd19y_RhluyAR0')
+handler = WebhookHandler('你的 Channel Secret')
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -30,26 +25,9 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    if event.message.text == '點歌':
-        reply_text = '請輸入歌名或歌詞以進行搜尋'
-    else:
-        reply_text = search_song(event.message.text)
-    
-    message = TextSendMessage(text=reply_text)
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 
-def search_song(keyword):
-    search_results = yt.search(keyword, filter='songs', limit=1)
-
-    if search_results:
-        video_id = search_results[0]['videoId']
-        video_url = f"https://www.youtube.com/watch?v={video_id}"
-        return f"找到符合條件的歌曲：\n{video_url}"
-    else:
-        return "找不到符合條件的歌曲"
-
-
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
