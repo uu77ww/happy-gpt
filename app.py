2415ae9062c6adaad88e2cd4d2fa6489c9f04a95
許昀暄 <126781189+uu77ww@users.noreply.github.com>
2023-06-13 18:49:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,50 @@
-from flask import Flask, request, abort
+import openai
+
+from flask_ngrok import run_with_ngrok   # colab 使用，本機環境請刪除
+from flask import Flask, request
+
+# 載入 LINE Message API 相關函式庫
 from linebot import LineBotApi, WebhookHandler
-from linebot.exceptions import InvalidSignatureError
-from linebot.models import *
+from linebot.models import TextSendMessage   # 載入 TextSendMessage 模組
+import json
 
 app = Flask(__name__)
 
-# 填入你的 Line Channel Access Token
-line_bot_api = LineBotApi('I86eRgDQEGoKvT5aXoefw9ekgJ4z2ACVKABcl9FK/JO1zbkWtwEYSjdzITlNNqSk0deRWhWVKGe3BvCef35jz7EDmBZVHTleu5/98I1CDuHP1IvglHx8t2YoT6WIu2Ica2E3TtEoftaprwIlEv4LOAdB04t89/1O/w1cDnyilFU=')
-
-# 填入你的 Line Channel Secret
-handler = WebhookHandler('c22f27f77d5d23fe3265b82966e7b02d')
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    signature = request.headers['X-Line-Signature']
+@app.route("/", methods=['POST'])
+def linebot():
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    json_data = json.loads(body)
+    print(json_data)
     try:
+        line_bot_api = LineBotApi('你的 Channel access token')
+        handler = WebhookHandler('你的 Channel secret ')
+        signature = request.headers['X-Line-Signature']
         handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
+        tk = json_data['events'][0]['replyToken']
+        msg = json_data['events'][0]['message']['text']
+        # 取出文字的前五個字元，轉換成小寫
+        ai_msg = msg[:6].lower()
+        reply_msg = ''
+        # 取出文字的前五個字元是 hi ai:
+        if ai_msg == 'hi ai:':
+            openai.api_key = '你的 OpenAI API Key'
+            # 將第六個字元之後的訊息發送給 OpenAI
+            response = openai.Completion.create(
+                model='text-davinci-003',
+                prompt=msg[6:],
+                max_tokens=256,
+                temperature=0.5,
+                )
+            # 接收到回覆訊息後，移除換行符號
+            reply_msg = response["choices"][0]["text"].replace('\n','')
+        else:
+            reply_msg = msg
+        text_message = TextSendMessage(text=reply_msg)
+        line_bot_api.reply_message(tk,text_message)
+    except:
+        print('error')
     return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token, message)
-
-
 if __name__ == "__main__":
+    run_with_ngrok(app)   # colab 使用，本機環境請刪除
     app.run()
