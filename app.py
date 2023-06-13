bf749271b68713dcb29c679839698054c00de2d7
許昀暄 <126781189+uu77ww@users.noreply.github.com>
2023-06-13 19:24:12 +0800

    Update and rename app.pyQ to app.py

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+import os
+import random
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
+handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    msg = event.message.text
+    if msg == "點歌":
+        song_list = ['歌曲1', '歌曲2', '歌曲3', '歌曲4', '歌曲5']
+        reply_msg = TextSendMessage(text=random.choice(song_list))
+    else:
+        reply_msg = TextSendMessage(text="請輸入「點歌」來點一首歌曲！")
+
+    line_bot_api.reply_message(event.reply_token, reply_msg)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
