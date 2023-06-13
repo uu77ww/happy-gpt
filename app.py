39cbfa7b00340d105c56f5379ff230eda58d024f
許昀暄 <126781189+uu77ww@users.noreply.github.com>
2023-06-13 18:41:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ from linebot.models import *
 app = Flask(__name__)
 
 # 填入你的 Line Channel Access Token
-line_bot_api = LineBotApi('你的 Channel Access Token')
+line_bot_api = LineBotApi('I86eRgDQEGoKvT5aXoefw9ekgJ4z2ACVKABcl9FK/JO1zbkWtwEYSjdzITlNNqSk0deRWhWVKGe3BvCef35jz7EDmBZVHTleu5/98I1CDuHP1IvglHx8t2YoT6WIu2Ica2E3TtEoftaprwIlEv4LOAdB04t89/1O/w1cDnyilFU=')
 
 # 填入你的 Line Channel Secret
-handler = WebhookHandler('你的 Channel Secret')
+handler = WebhookHandler('c22f27f77d5d23fe3265b82966e7b02d')
 
 @app.route("/callback", methods=['POST'])
 def callback():
